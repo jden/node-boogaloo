91d973fa7511cb5514fbbb1f8eaf72e5934bbdb4
jden <jason@denizac.org>
2014-06-14 17:04:29 -0700

    implement

--- index.js
+++ index.js
@@ -1,5 +1,111 @@
-function boogaloo () {
-  
+var fs = require('fs')
+var path = require('path')
+var marked = require('marked')
+var package = require('./package.json')
+
+function map(obj, fn) {
+  return Object.keys(obj)
+    .map(function (key) {
+      return fn(obj[key], key)
+    })
 }
+
+
+function boogaloo (directory, callback) {
+  getSources(directory, function (err, slides) {
+    if (err) { return callback(err) }
+    if (slides.style) {
+      var style = renderStyle(slides.style.files[0])
+      delete slides.style
+    }
+    var rendered = map(slides, function (slide, id) {
+      var renderedFiles = slide.files.map(function (file) {
+        switch (file.extension) {
+          case '.md':
+            return renderSlide(file)
+          case '.jpg':
+          case '.jpeg':
+            return renderImage(file)
+        }  
+      })
+      return wrapSlide(id, renderedFiles.join('\n'))
+    })
+
+    var page = wrapPage({
+      title: 'Slide Show',
+      style: style,
+      slides: rendered.join(''),
+      generator: package.name + ' ' + package.version,
+      repository: 'githubz'
+    })
+    callback(null, page)
+  })
+}
+
+function getSources(directory, callback) {
+  return fs.readdir(directory, function (err, files) {
+    if (err) { return callback(err) }
+    var resources = files.map(function (file) {
+        var ext = path.extname(file)
+        return {
+          directory: directory,
+          path: path.join(directory, file),
+          name: path.basename(file, ext),
+          extension: ext.toLowerCase()
+        }
+      })
+      .reduce(groupBySlide, {})
+    callback(null, resources)
+  })
+}
+
+function groupBySlide(resources, file) {
+  resources[file.name] = resources[file.name] || {files: []}
+  resources[file.name].files.push(file)
+  return resources
+}
+
+function renderImage (file) {
+  // todo: address multiple mime types
+  var data = 'data:image/jpeg;base64,'
+  // todo: streamify
+  var raw = fs.readFileSync(file.path).toString('base64')
+  data += raw
+  var html = '<img src="' + data + '" />'
+  // var html = '<div class="img" style="background: url("'+data+'");"></div>'
+  return html
+}
+
+function renderStyle(file) {
+  var raw = fs.readFileSync(file.path).toString()
+  return '<style>' + raw + '</style>'
+}
+
+function renderSlide (file) {
+  var raw = fs.readFileSync(file.path).toString()
+  return '<div class="contents">' + marked(raw) + '</div>'
+}
+
+function wrapSlide(id, contents) {
+  var cssClass = id == 1 ? 'slide active' : 'slide'
+  var html = '<div id="slide' + id + '" class="' + cssClass + '">' + contents + '</div>\n'
+  return html
+}
+
+function wrapPage(contents) {
+  var template = fs.readFileSync('./index.html').toString()
+  template = template.replace('{{slides}}', contents.slides || '')
+  template = template.replace('{{title}}', contents.title || '')
+  template = template.replace('{{style}}', contents.style || '')
+  template = template.replace('{{generator}}', contents.generator || '')
+  template = template.replace('{{repository}}', contents.repository || '')
+  return template
+}
+
+boogaloo('./demo', function (err, value) {
+  if (err) { return console.error(err) }
+  fs.writeFileSync('./out.html', value)
+  console.log('wrote out.html')
+})
 
 module.exports = boogaloo
---
+++ show.js
@@ -0,0 +1,89 @@
+// todo use browserify for this whole mess
+
+function toArray(arr) {
+  return Array.prototype.slice.call(arr)
+}
+
+// parse hash fragment thing &
+// restore active slide
+var slideIds = toArray(document.querySelectorAll('.slide'))
+  .map(function (slide) {
+    return parseInt(slide.id.replace(/[^\d]*/g,''))
+  })
+var activeSlideId
+
+showSlide(parseInt(getSlideIdFromHash() || document.querySelector('.active').id.replace(/[^\d]*/g,'')) || 1)
+
+function getSlideIdFromHash() {
+  return parseInt(window.location.hash.substr(1, window.location.hash.indexOf('/')-1))
+}
+
+// bind keys for next / prev
+
+document.addEventListener('keypress', function (e) {
+  switch (e.key) {
+    case 'Left':
+    case 'PageUp':
+      changeSlide(-1)
+      break
+    case 'Right':
+    case 'PageDown':
+      changeSlide(1)
+      break
+  }
+})
+
+document.addEventListener('click', function (e) {
+  if (e.clientX > (document.querySelector('.active').offsetWidth / 2)) {
+    // click right side
+    changeSlide(1)
+  } else {
+    changeSlide(-1)
+  }
+})
+
+window.addEventListener('hashchange', function (e) {
+  var id = getSlideIdFromHash()
+  if (id == activeSlideId) { return }
+  console.log('wat')
+  showSlide(id)
+})
+
+function showSlide(id) {
+  console.log()
+  if (typeof id !== 'number' || Number.isNaN(id) || id === activeSlideId) { return }
+  console.log('=======\nSlide ' + id + ' / ' + slideIds.length)
+  if (activeSlideId) { document.getElementById('slide'+activeSlideId).classList.remove('active') }
+  document.getElementById('slide'+id).classList.add('active')
+  activeSlideId = id
+  document.location.hash = id+'/'+slideIds.length
+  console.log(document.getElementById('slide'+id).textContent)
+}
+
+function changeSlide(relative) {
+  var next = limit(activeSlideId + relative, 1, slideIds.length)
+  showSlide(next)
+}
+
+function limit(num, min, max) {
+  return Math.min(Math.max(num, min), max)
+}
+
+function adjustImageRatios() {
+  var docX = document.querySelector('.active').offsetWidth
+  var docY = document.querySelector('.active').offsetHeight
+  var images = toArray(document.querySelectorAll('.slide > img'))
+  images.forEach(function (img) {
+    // we want to make the smallest dimension be 100% of screen width
+    if (img.width > img.height) {
+      img.style.height = '100%'
+    }
+    else {
+      img.style.width = '100%'
+    }
+
+    // console.log(ratio, imgRatio, img)
+  })
+}
+
+// adjustImageRatios()
